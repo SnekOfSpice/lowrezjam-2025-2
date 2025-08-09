f811351ab6e960c99ef58178ac9dcdff47cf0553
Fio <leukert.fio@gmail.com>
2025-08-09 14:16:51 +0200

    awawa things are kinda broken but theworks!!

--- game/src/stencil/stencil.gd
+++ game/src/stencil/stencil.gd
@@ -2,7 +2,25 @@ extends Sprite2D
 class_name Stencil
 
 signal start_level
+signal entered_pixel(coord:Vector2i)
+
+var visited_pixels := []
+
+var last_pos := Vector2i(-100, -100)
 
 func _process(delta: float) -> void:
-	if get_local_mouse_position() == $StartMarker.position:
+	var mouse_pos := Vector2i(get_local_mouse_position())
+	mouse_pos.x = clampi(mouse_pos.x, -31, 31)
+	mouse_pos.y = clampi(mouse_pos.y, -31, 31)
+	var marker_pos := Vector2i($StartMarker.position) + Vector2i.ONE # idk why but the ONE is necessary
+	
+	if mouse_pos == marker_pos:
 		emit_signal("start_level")
+	
+	if last_pos != mouse_pos and not visited_pixels.has(mouse_pos):
+		emit_signal("entered_pixel", mouse_pos)
+		visited_pixels.append(mouse_pos)
+		print("added" , mouse_pos)
+	
+	last_pos = mouse_pos
+	printt(mouse_pos, marker_pos, visited_pixels.size())
--- main.gd
+++ main.gd
@@ -4,11 +4,18 @@ const LEVELS := [
 	"utah"
 ]
 
+## used for dev purposes
+@export var skip_notifications := false
+
 var level : String
 var level_counter := 0
+var level_started := false
 
 var show_time_per_character := 0.04
 var hide_time_per_character := 0.01
+
+var goal_pixels := []
+var hit_pixels := []
 
 func _ready() -> void:
 	%Notification.visible_characters = 0
@@ -24,17 +31,53 @@ func place_level(number:int):
 		child.queue_free()
 	
 	var level_path := "res://game/src/stencil/stencils/%s.tscn" % LEVELS[number]
-	var instance : Stencil = load(level_path).instantiate()
-	%LevelContainer.add_child(instance)
-	instance.start_level.connect(start_level)
+	var stencil : Stencil = load(level_path).instantiate()
+	%LevelContainer.add_child(stencil)
+	stencil.start_level.connect(on_stencil_start_level)
+	stencil.start_level.connect(on_stencil_pixel_entered)
+	
+	goal_pixels.clear()
+	hit_pixels.clear()
+	for x in 64:
+		for y in 64:
+			var coord = Vector2(x, y)
+			if stencil.is_pixel_opaque(coord):
+				goal_pixels.append(Vector2i(coord))
 	
 	notify(["MOUSE 2 CROSS"], 1)
+	level_started = false
+	print(goal_pixels)
 
-func start_level():
+func on_stencil_pixel_entered(coord:Vector2i):
+	if hit_pixels.has(coord):
+		return
+	hit_pixels.append(coord)
+	goal_pixels.erase(coord)
+	print(goal_pixels)
+	if goal_pixels.is_empty():
+		finish_level()
+
+func finish_level():
+	print("you win")
+
+func get_current_stencil() -> Stencil:
+	if %LevelContainer.get_child(0) is Stencil:
+		return %LevelContainer.get_child(0)
+	return null
+
+func on_stencil_start_level():
+	if level_started:
+		return
+	level_started = true
 	print("start")
 
 var notification_tween
 func notify(messages:Array, initial_delay:=0.0, callable_at_end:=Callable()):
+	if skip_notifications:
+		if callable_at_end:
+			callable_at_end.call()
+		push_warning("toggle skip_notifications before exporting")
+		return
 	if notification_tween:
 		notification_tween.kill()
 	notification_tween = create_tween()
